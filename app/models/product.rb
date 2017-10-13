7ab98193619b6e0c488ad6c3fa5b2b903f6ad1dd
J-Cunha <soares.cunha.joao@gmail.com>
2017-10-12 23:28:57 -0300

    Update product validations

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,14 +1,14 @@
 class Product < ApplicationRecord
-  belongs_to :category, required: false
+  belongs_to :category
   has_many :product_flavors, dependent: :destroy
   has_many :flavors, dependent: :destroy, through: :product_flavors
 
   has_many :product_sizes, dependent: :destroy
   accepts_nested_attributes_for :product_sizes, reject_if: :all_blank, allow_destroy: true
- # validates_uniqueness_of :name
+  validates_uniqueness_of :name
   mount_uploader :product_image, ProductImageUploader
   crop_uploaded :product_image
-  validates_presence_of :name, :category, :description
+  validates_presence_of :name, :category, :product_image,:product_sizes, :description
   def min_value
     unless self.product_sizes.empty?
     "R$ #{(('%.2f' %  self.product_sizes.minimum(:value)).sub'.',',')}"
